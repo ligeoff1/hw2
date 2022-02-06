36f68eca7bf306828de7ca6b9754d491348ed605
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-06 08:15:29 +0000

    create movie table

--- db/migrate/20220206080004_create_movies.rb
+++ db/migrate/20220206080004_create_movies.rb
@@ -1,7 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
-
+      t.integer :id
+      t.string :title
+      t.integer :year_released
+      t.string :rated
+      t.integer :person_id
       t.timestamps
     end
   end
